8301187a4899a8a4523470708aa4dddaa078bc64
van2kot <117808297+van2kot@users.noreply.github.com>
2025-04-21 10:04:55 +0000

    datatypes

---
+++ basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 12321
+let accountEmail ="jahs@gmail.com "
+let accountPassword = "123456"
+// accountEmail ="hasf@gmail.com"
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword])
+
---
+++ basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all JS as newer version
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object                 
